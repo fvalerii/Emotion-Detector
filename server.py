5189f1ea1076574bdf82168dd92d869d1e1c321c
fvalerii <fvalerii@gmail.com>
2025-07-13 05:49:11 -0400

    task 8 static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,9 @@
+'''
+Emotion Detector Web Application
+Flask app that acesses the Watson AI embedded NLP library for emotion detetion in
+a user provided text
+'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,16 +11,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    '''
+    Render content of index.html to provide the main index page 
+    '''
     return render_template('index.html')
 
 @app.route('/emotionDetector', methods=['GET'])
 def detect():
+    '''
+    Handles GET requests to analye emotions of input text.
+    Returns: summary of emotion scores and dominant emotion.
+    If the input text was empty returns error message
+    '''
     statement = request.args.get('textToAnalyze')
     output = emotion_detector(statement)
     dominant_emotion = output['dominant_emotion']
 
-    if dominant_emotion == None:
-        return "Invalid text! Please try again!" 
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
     emotions = ", ".join(
         f"'{key}': {value}"
         for key, value in output.items()
@@ -25,4 +39,4 @@ def detect():
     return response
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)    
+    app.run(host="0.0.0.0", port=5000)
